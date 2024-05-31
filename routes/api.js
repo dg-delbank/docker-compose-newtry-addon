b3ed263ed10e49720f21923b56def88cde97e762
dg-delbank <165733779+dg-delbank@users.noreply.github.com>
2024-05-31 12:42:13 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const OtherModel = require('../models/OtherModel');
 
 router.get('/other', (req, res) => {
+  console.log("Received request for /other");
   res.status(200).send('Second API is working!');
 });
 
 router.get('/others', (req, res) => {
+  console.log("Received request for /others");
   res.status(200).send('Second API is working! Amem!');
 });
 
