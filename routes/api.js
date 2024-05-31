69baa56e06a28082359db493e1fc22655df46497
dg-delbank <165733779+dg-delbank@users.noreply.github.com>
2024-05-31 12:51:50 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const OtherModel = require('../models/OtherModel');
 
-router.get('/other', (req, res) => {
+router.get('/api2/other', (req, res) => {
   console.log("Received request for /other");
   res.status(200).send('Second API is working!');
 });
 
-router.get('/others', (req, res) => {
+router.get('/api2/others', (req, res) => {
   console.log("Received request for /others");
   res.status(200).send('Second API is working! Amem!');
 });
